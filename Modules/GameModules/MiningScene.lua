8a3cb1ed113ea45e59f78a7cd11fde6519afd87e
AVZhelyazkov <avzhelyazkov18@gmail.com>
2023-03-15 23:41:32 +0200

    Major Update

---
+++ Modules/GameModules/MiningScene.lua
@@ -0,0 +1,31 @@
+MiningScene = {}
+
+local SceneRequest = nil
+
+function MiningScene:RequestSceneChange()
+    if SceneRequest then
+        return SceneRequest
+    end
+end
+
+function MiningScene:draw()
+    
+end
+
+function MiningScene:keypressed()
+
+end
+
+function MiningScene:keyreleased()
+
+end
+
+function MiningScene:mousepressed()
+end
+function MiningScene:mousereleased()
+end
+
+function MiningScene:update()
+
+end
+return MiningScene
